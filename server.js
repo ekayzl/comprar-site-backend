b47dd0d78fe038404258a40847e7e7a47b4d273b
ekayzl <suporte.ekay@gmail.com>
2025-04-13 02:05:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.post('/api/pagar', async (req, res) => {
 });
 
 const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
